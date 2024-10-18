2d087993b85fbe394ed438ba89a71c9f1eb2b06b
zhqmfkv <zhqmfkv@naver.com>
2024-10-18 11:14:50 +0900

    [FEAT] header 수정

--- src/main/groovy/io/hhplus/tdd/hhplusconcertjava/common/config/SwaggerConfig.java
+++ src/main/groovy/io/hhplus/tdd/hhplusconcertjava/common/config/SwaggerConfig.java
@@ -1,5 +1,9 @@
 package io.hhplus.tdd.hhplusconcertjava.common.config;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.Paths;
@@ -8,6 +12,19 @@ import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@SecurityScheme(
+        type = SecuritySchemeType.APIKEY,
+        name = "Authorization",
+        description = "userId 룰 입력해주세요.",
+        in = SecuritySchemeIn.HEADER)
+@SecurityScheme(
+        type = SecuritySchemeType.APIKEY,
+        name = "token",
+        description = "uuid을 입력해주세요",
+        in = SecuritySchemeIn.HEADER)
 @Configuration
 public class SwaggerConfig {
 
@@ -15,7 +32,15 @@ public class SwaggerConfig {
     public OpenAPI openAPI() {
         return new OpenAPI()
                 .components(new Components())
+                .security(getSecurityRequirement())
                 .info(apiInfo());
+    }
+
+    private List<SecurityRequirement> getSecurityRequirement() {
+        List<SecurityRequirement> requirements = new ArrayList<>();
+        requirements.add(new SecurityRequirement().addList("Authorization"));
+        requirements.add(new SecurityRequirement().addList("token"));
+        return requirements;
     }
 
     private Info apiInfo() {
--- src/main/groovy/io/hhplus/tdd/hhplusconcertjava/wait/interfaces/IWaitController.java
+++ src/main/groovy/io/hhplus/tdd/hhplusconcertjava/wait/interfaces/IWaitController.java
@@ -1,6 +1,7 @@
 package io.hhplus.tdd.hhplusconcertjava.wait.interfaces;
 
 import io.hhplus.tdd.hhplusconcertjava.wait.interfaces.dto.GetTokenResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -11,7 +12,7 @@ import java.util.Map;
 @Tag(name = "대기열 API", description = "대기열 관련 Controller 입니다.")
 public interface IWaitController {
 
-
+    
     public GetTokenResponseDto getWaitToken(@RequestHeader Map<String, String> header, @RequestParam Map<String, String> params);
 
 }
