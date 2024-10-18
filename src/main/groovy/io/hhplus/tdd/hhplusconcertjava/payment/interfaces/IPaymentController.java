860f751ff15c4e68e39e0610c86a3afeab4d5ad5
zhqmfkv <zhqmfkv@naver.com>
2024-10-18 11:00:34 +0900

    [FEAT] swagger Image 첨부

--- src/main/groovy/io/hhplus/tdd/hhplusconcertjava/payment/interfaces/IPaymentController.java
+++ src/main/groovy/io/hhplus/tdd/hhplusconcertjava/payment/interfaces/IPaymentController.java
@@ -2,7 +2,9 @@ package io.hhplus.tdd.hhplusconcertjava.payment.interfaces;
 
 import io.hhplus.tdd.hhplusconcertjava.payment.interfaces.dto.PostPayReservationRequestDto;
 import io.hhplus.tdd.hhplusconcertjava.payment.interfaces.dto.PostPayReservationResponseDto;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
+@Tag(name="결제 API", description = "결제 관련 API 입니다.")
 public interface IPaymentController {
     public PostPayReservationResponseDto postPayReservation(PostPayReservationRequestDto postPayReservationRequestDto);
 
